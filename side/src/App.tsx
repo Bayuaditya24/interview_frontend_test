ee8758886a2ac18220e5f896fc9c71c5d364404e
I Putu Bayu Aditya Putra <49913455+Bayuaditya24@users.noreply.github.com>
2025-05-09 08:48:07 +0800

    Update App.tsx

--- side/src/App.tsx
+++ side/src/App.tsx
@@ -7,13 +7,12 @@ import AddQuestion from "./pages/AddQuestion";
 import Login from "./pages/Login";
 import PrivateRoute from "./components/PrivateRoute";
 import FormDetail from "./pages/FormDetail";
-import FormResponse from "./pages/FormResponse";
 import AnswerForm from "./pages/AnswerForm";
 
 const App: React.FC = () => {
   return (
     <Routes>
-      {/* Redirect root "/" ke Home dalam layout utama jika sudah login */}
+      
       <Route
         path="/"
         element={
@@ -25,7 +24,6 @@ const App: React.FC = () => {
         <Route index element={<Home />} />
         <Route path="form" element={<FormCreate />} />
         <Route path="detail/:slug" element={<FormDetail />} />
-        <Route path="/response/:slug" element={<FormResponse />} />
         <Route path="/forms/:slug/answer" element={<AnswerForm />} />
         <Route path="forms/:slug/add-question" element={<AddQuestion />} />
       </Route>
