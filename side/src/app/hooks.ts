c5ce6d638497492b20b50b86a2908b03aeb8613c
I Putu Bayu Aditya Putra <49913455+Bayuaditya24@users.noreply.github.com>
2025-05-09 08:27:36 +0800

    Update hooks.ts

--- side/src/app/hooks.ts
+++ side/src/app/hooks.ts
@@ -2,6 +2,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "./store";
 
-// Gunakan ini di seluruh aplikasi untuk type safety
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
